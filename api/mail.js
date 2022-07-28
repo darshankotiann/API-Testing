97161f76895df612b2f4013ac9c01c0151dfe485
Darshan167 <67037146+Darshan167@users.noreply.github.com>
2022-07-28 19:07:59 +0530

    Testing

--- api/mail.js
+++ api/mail.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const router=express.Router();
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-module.exports=router
+const express = require("express");
+const router = express.Router();
+router.get("/", async (req, res) => {
+  try {
+    res.send("Hello World!");
+  } catch {
+    return res.status(500).send("server error");
+  }
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const hello = require("./api/mail");
 app.use("/api/mail", hello);
-
+app.use(express.json({ extended: false }));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
